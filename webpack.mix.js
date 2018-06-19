a0bf781d0a67a1ce287f8775fceccf149e7b4963
weichengcing <weichengcing@kencnc.com>
2018-06-19 08:36:47 +0800

    表單開關增加自訂文字樣式

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@ var mix = require('laravel-mix');
 
 mix.sass('./src/sass/styles.scss', './dist/css/styles.css')
     .copy('./src/js', './dist/js')
-    .copy('./src/imgs', './dist/imgs');
+    .copy('./src/imgs', './dist/imgs').sourceMaps();
