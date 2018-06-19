b7e298d0600adf154a472ad92ae528282302c2b4
weichengcing <weichengcing@kencnc.com>
2018-06-19 09:17:42 +0800

    加入之前寫的樣式表

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@ var mix = require('laravel-mix');
 
 mix.sass('./src/sass/styles.scss', './dist/css/styles.css')
     .copy('./src/js', './dist/js')
-    .copy('./src/imgs', './dist/imgs').sourceMaps();
+    .copy('./src/imgs', './dist/imgs');
